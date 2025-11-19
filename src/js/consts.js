ec2c6d2b69b5169b4cc254664d599b84da52510c
vitmord <vitmord@gmail.com>
2025-11-19 22:42:25 +0300

    dev: lesson 7, ready for dev

--- src/js/consts.js
+++ src/js/consts.js
@@ -1,4 +1,3 @@
-// export const WORDS = ['code', 'frontend', 'programming']
-export const WORDS = ['abc']
+export const WORDS = ['code', 'frontend', 'programming']
 
 export const KEYBOARD_LETTERS = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
